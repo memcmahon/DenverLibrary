d4c593efcfdcf8b59e7970623b0fcc95bd29edbd
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-04-12 12:57:12 -0600

    Update Branch.cs

--- DenverLibrary/Branch.cs
+++ DenverLibrary/Branch.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DenverLibrary
 {
-    internal class Branch
+    public class Branch
     {
         public string Name { get; set; }
         public List<Book> Books { get; set; } = new List<Book>();
