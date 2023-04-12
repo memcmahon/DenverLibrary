4c30185b03aecc875ac6a7f4fb2148b7d42582cd
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-04-12 12:56:50 -0600

    Update Book.cs

--- DenverLibrary/Book.cs
+++ DenverLibrary/Book.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DenverLibrary
 {
-    internal class Book
+    public class Book
     {
         public string Title { get; set; }
         public string Author { get; set; }
